4d8be3ed3851d54e025d18fa7bae1ccf17fbabf1
KonstantinRog <k.rogozyanskij@sigma-it.ru>
2022-09-19 17:15:53 +0300

    Lesson2

---
+++ Lesson 2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    
+    String sex = "male";
+    String name = "Konstantin";
+    int height = 182;
+    int weight = 75;
+    int age = 26;
+
+    public void move() {
+        System.out.println("Идти");
+    }
+
+    public void sit() {
+        System.out.println("Сидеть");
+    }
+
+    public void run() {
+        System.out.println("Бежать");
+    }
+
+    public void say() {
+        System.out.println("Говорить");
+    }
+
+    public void learnJava() {
+        System.out.println("Учить java");
+    }
+}
